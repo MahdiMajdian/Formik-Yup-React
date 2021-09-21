94927ea6acb2a75553c0f6349d4a22f1365a47ac
Mahdi Majdian <mahdi2011.shz@gmail.com>
2021-09-21 10:03:56 +0430

    added image and made responsive

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css"
 import SignUp from "./components/SignUp"
-
+import img from "../src/assets/images/intro.svg"
 function App() {
 	return (
-		<div className=" grid grid-cols-12 h-screen">
+		<div className=" grid grid-cols-1 lg:grid-cols-12 h-screen">
 			<div className="col-span-5 ">
 				<SignUp />
 			</div>
-			<div className="col-span-7 bg-gray-200 flex items-center justify-center">
-				SOME IMAGES
+			<div className="col-span-7 lg:bg-gray-200 p-8 flex items-center justify-center">
+				<div className="max-w-md">
+					<img src={img} />
+				</div>
 			</div>
 		</div>
 	)
