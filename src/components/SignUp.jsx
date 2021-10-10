cf4ba1d06467bfcf7400b6017298c2a3d6fe9c0c
Mahdi Majdian <mahdi2011.shz@gmail.com>
2021-10-10 10:56:18 +0330

    add disable state to submit button add hiden text to fill the error message spot while it's not needed

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 import TextInput from './TextInput'
 import * as Yup from 'yup'
 const SignUp = () => {
-    const validate = Yup.object({
+    const validate = Yup.object().shape({
         firstName: Yup.string().max(20, 'first name should be less than 20 characters').required('first name is required'),
         lastName: Yup.string().max(25, 'last name must be less than 25 chars').required('lastname field must be filled'),
         email: Yup.string().email('invalid email').required('email field must not be empty'),
         password: Yup.string().min(6, 'password must be at least 6 chars or more').required('Password field must be filled'),
-        confirmPassword: Yup.string().oneOf([Yup.ref('password'), null], 'passwords must be the same').required('Password field must be filled'),
+        confirmPassword: Yup.string().oneOf([Yup.ref('password')], 'passwords must be the same').required('Password field must be filled'),
     })
     return (
         <Formik
@@ -21,17 +21,17 @@ const SignUp = () => {
             }}
             validationSchema={validate}
             onSubmit={val => console.log(val)}>
-            {formik => (
-                <div className='p-8'>
+            {({ dirty, isValid }) => (
+                <div className='p-8 px-16'>
                     <h1 className='mb-8 font-bold text-5xl text-gray-600'>Sign Up</h1>
-                    <Form className='px-8'>
+                    <Form>
                         <TextInput label="firstname" name='firstName' type='text' />
                         <TextInput label="lastname" name='lastName' type='text' />
                         <TextInput label="email" name='email' type='email' />
                         <TextInput label="password" name='password' type='password' />
                         <TextInput label="confirm Password" name='confirmPassword' type='password' />
-                        <button type='submit' className={`rounded bg-green-500 hover:bg-green-600 mr-4 px-4 py-2 text-white font-medium`}>Submit</button>
-                        <button type='reset' className={`rounded text-red-600 hover:text-white hover:bg-red-600 mr-4 px-4 py-2 border border-red-600 font-medium`}>Reset</button>
+                        <button type='submit' disabled={!dirty || !isValid} className={`rounded bg-green-500 hover:bg-green-600 disabled:bg-green-300 mt-4 mr-4 px-4 py-2 text-white font-medium`}>Submit</button>
+                        <button type='reset' className={`rounded text-red-600 hover:text-white hover:bg-red-600 mt-4 mr-4 px-4 py-2 border border-red-600 font-medium`}>Reset</button>
                     </Form>
                 </div>
             )}
--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -2,12 +2,13 @@ import { ErrorMessage, useField } from 'formik'
 import React from 'react'
 
 const TextInput = (props) => {
-    const [field, meta] = useField(props)
+    const [field, meta] = useField(props.name)
     return (
-        <div className='mb-4'>
+        <div className=''>
             <label htmlFor={field.name} className='font-medium text-gray-600 capitalize'>{props.label}</label>
-            <input type={props.type} id={field.name} autoComplete="off" {...field} {...props}
+            <input id={field.name} autoComplete="off" {...field} {...props}
                 className={`border w-full p-2 mt-2 border-gray-300 focus:outline-none focus:ring-1 rounded ${meta.touched && meta.error && 'border-red-600 focus:ring-red-500'}`} />
+            {(!meta.error || !meta.touched) && <div className='text-sm font-medium text-transparent'>what u doing here?</div>}
             <ErrorMessage component='div' className='text-red-500 text-sm font-medium' name={field.name} />
         </div>
     )
